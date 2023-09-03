c4a0d64f570a64d823e6f8e7aca867733e5e68f0
marcs-feh <marcosfehlauer@protonmail.com>
2023-09-03 20:20:38 -0300

    add limits

--- types.hpp
+++ types.hpp
@@ -26,6 +26,25 @@ using byte    = unsigned char;
 using rune    = uint32_t;
 using uintptr = uintptr_t;
 
+namespace core {
+
+constexpr u8  u8_max  = 0xff;
+constexpr u16 u16_max = 0xffff;
+constexpr u32 u32_max = 0xffff'ffffULL;
+constexpr u64 u64_max = 0xffff'ffff'ffff'ffffULL;
+
+constexpr i8  i8_max  = 0x7f;
+constexpr i16 i16_max = 0x7fff;
+constexpr i32 i32_max = 0x7fff'ffffLL;
+constexpr i64 i64_max = 0x7fff'ffff'ffff'ffffLL;
+
+constexpr i8  i8_min  = -0x80;
+constexpr i16 i16_min = -0x8000;
+constexpr i32 i32_min = -0x8000'0000LL;
+constexpr i64 i64_min = -0x8000'0000'0000'0000LL;
+
+}
+
 static_assert(sizeof(float) == 4, "`float` must be 32 bit");
 static_assert(sizeof(double) == 8, "`double` must be 64 bit");
 static_assert(sizeof(ptrdiff_t) >= sizeof(size_t), "`ssize` must be at least same size as size_t");
